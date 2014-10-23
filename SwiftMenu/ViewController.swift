72e8c068a3093e50e9d1e28a91ecf91ca3e112b3
魏仕月 <w_shiyue@163.com>
2014-10-23 18:06:35 +0800

    swift menu

---
+++ SwiftMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMenu
+//
+//  Created by syweic on 14/10/23.
+//  Copyright (c) 2014年 ___iSoftStone___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
